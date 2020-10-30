cabbb5e09b092942371fd7e08608ee1eb9302bac
Mlucio94 <mlucio94@gmail.com>
2020-10-30 17:54:06 -0300

    modif promise y useEfect en conteiner list

--- src/components/card.js
+++ src/components/card.js
@@ -2,20 +2,21 @@ import React from 'react';
 import ItemCount from './itemCount';
 
 
-function Card({image,CardTitle}){
+function Card({id,image,prodName,prodDesc,price,stock}){
     return <>
-        <div class="card ">
-            <img src="https://http2.mlstatic.com/D_NQ_NP_734011-MLA43411448205_092020-O.webp" class="card-img-top" alt="..."/>
+        <div class="card col-md-3">
+           
+            <img src={image} class="card-img-top" alt="..."/>
             <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <h5 class="card-title">{prodName}</h5>
+            <p class="card-text">{prodDesc}</p>
             </div>
             <div class="card-footer containter">
                 <div class="row justify-content-between">
-                    <h3 class="float-left  text-center text-info col-md">$ 999</h3>
+                    <h3 class="float-left  text-center text-info col-md">{price}</h3>
                     <div class="col-md">
                      <ItemCount
-                        stock={10}
+                        stock={stock}
                         initial={0}
                         onAdd={cant => alert(`Agregados al carrito: ${cant}`)}
                      /> 
--- src/components/itemListContainer.js
+++ src/components/itemListContainer.js
@@ -1,12 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from './card';
 
-function ItemListContainer({Title}){
+const getItems = () =>{
+        
+    return new Promise((res,rej)=>{
+        setTimeout(()=>{
+            res(
+                [
+                    {   id:0,
+                        image:"https://http2.mlstatic.com/D_NQ_NP_749088-MLA43301556023_082020-O.webp",
+                        prodName:"Producto 1",
+                        prodDesc:"Este es el primer producto",
+                        price:"$999",
+                        stock:20
+                    },
+                    {   id:1,
+                        image:"https://http2.mlstatic.com/D_NQ_NP_749088-MLA43301556023_082020-O.webp",
+                        prodName:"Producto 2",
+                        prodDesc:"Este es el segundo producto",
+                        price:"$995",
+                        stock:20,
+                    },
+                    {   id:2,
+                        image:"https://http2.mlstatic.com/D_NQ_NP_749088-MLA43301556023_082020-O.webp",
+                        prodName:"Producto 3",
+                        prodDesc:"Este es el tercer producto",
+                        price:"$999",
+                        stock:20
+                    },
+                    {   id:3,
+                        image:"https://http2.mlstatic.com/D_NQ_NP_749088-MLA43301556023_082020-O.webp",
+                        prodName:"Producto 4",
+                        prodDesc:"Este es el cuarto producto",
+                        price:"$99",
+                        stock:20
+                    },
+                    {   id:4,
+                        image:"https://http2.mlstatic.com/D_NQ_NP_749088-MLA43301556023_082020-O.webp",
+                        prodName:"Producto 5",
+                        prodDesc:"Este es el quinto producto",
+                        price:"$999",
+                        stock:20
+                    },
+                ]
+            );
+        },2000)
+    
+    });
+}
 
+function ItemListContainer({Title}){
+    const [items,setItems]=useState(null);
+
+    useEffect(()=>{
+        console.log('iniciando conteiner');
+        getItems().then(items=>{
+            setItems(items);
+        })
+    })  
     return <>
         <div class="container-md mt-4 mb-4">
             <h2>{Title}</h2>
             <div class="card-deck mt-4">
+                {items && <Card items={items}></Card>}
+            </div>
+            {/* <div class="card-deck mt-4">
                 <Card/>
                 <Card/>
                 <Card/>
@@ -17,7 +75,7 @@ function ItemListContainer({Title}){
                 <Card/>
                 <Card/>
                 <Card/>
-            </div>
+            </div> */}
         </div>
     </>
 
